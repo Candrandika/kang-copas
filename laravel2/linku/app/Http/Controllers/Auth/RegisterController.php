ab4f370c26a1ae3b3ab060bb821db2b5232dc9cd
Candra Andika Putra <candrandikarpl1.kanesa@gmail.com>
2022-03-17 11:23:45 +0700

    h

--- laravel2/linku/app/Http/Controllers/Auth/RegisterController.php
+++ laravel2/linku/app/Http/Controllers/Auth/RegisterController.php
@@ -41,6 +41,10 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+    public function index(){
+        return view('login/register');
+    }
+
     /**
      * Get a validator for an incoming registration request.
      *
--- laravel2/linku/routes/web.php
+++ laravel2/linku/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\RegisterController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +25,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
